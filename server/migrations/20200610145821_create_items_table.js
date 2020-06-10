48d9b74aeab958b3df0074e8a763b8e298bad583
parkhank <park.hty@gmail.com>
2020-06-10 18:03:50 -0400

    fix database item table columns, finish item seed values

--- server/migrations/20200610145821_create_items_table.js
+++ server/migrations/20200610145821_create_items_table.js
@@ -3,9 +3,9 @@ exports.up = knex => {
   return knex.schema.createTable("items", table => {
     table.integer("id").primary();
     table.string("name").notNullable();
-    table.string("unit").notNullable().defaultTo("case");
-    table.decimal("kgPerUnit", 5, 2).notNullable();
-    table.decimal("pricePerUnit", 5, 2).notNullable();
+    table.decimal("cost", 5, 2).notNullable();
+    table.integer("unitsPerCase").nullable();
+    table.decimal("kgPerCase").nullable();
   })
 };
 
