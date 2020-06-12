99e2e58c9785dd75c3bd07b649c44782a239b656
parkhank <park.hty@gmail.com>
2020-06-12 14:09:31 -0400

    promise chain for all data in inventory.js

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const Inventory = require("../models/inventory");
+const Item = require("../models/items");
+const Location = require("../models/locations");
 const router = express.Router();
 
 router
@@ -8,6 +10,21 @@ router
   Inventory
     .fetchAll()
     .then(inventories => {
+      return(
+        Item.fetchAll()
+        .then(items => {
+          return(
+            Location.fetchAll()
+            .then(locations => {
+              return [items, inventories, locations]
+            })
+          )
+        })
+      )
+    })
+    .then(([items, inventories, locations]) => {
+      // create an object of inventory lines with item data
+      // res.status(200).json(newObject);
       res.status(200).json(inventories);
     })
 })
