ada31e4a1de318972171700e4a452cbe703075a0
parkhank <park.hty@gmail.com>
2020-06-20 09:43:08 -0400

    unify background styling, redesign invoice form + ID display

--- client/src/pages/Invoice/Invoice.js
+++ client/src/pages/Invoice/Invoice.js
@@ -9,6 +9,9 @@ class Invoice extends React.Component {
 state = {
   locationList: [],
   itemList: [],
+  item_id: "Item ID",
+  location_id: "",
+  cases: '',
 }
 
 changeHandle = (e, id) => {
@@ -28,6 +31,11 @@ addCount = async (e) => {
     item_id: this.state.item_id,
     location_id: this.state.location_id,
     cases: this.state.cases,
+  })
+  await this.setState({
+    item_id: "",
+    location_id: "",
+    cases: null,
   })
 }
 
@@ -54,50 +62,59 @@ render() {
       <form
         className="invoice__form"
         onSubmit={this.addCount}>
-        <select
-          className="invoice__form-item"
-          name="item"
-          onChange={e => {this.changeHandle(e, "item_id")}}>
-          <option value="">Choose an item:</option>
-          {
-            this.state.itemList.map(item => {
-              return(
-              <option
-                key={item.id}
-                value={item.id}>
-                {item.name}
-              </option>
-              )
-            })
-          }
-        </select>
-        <select
-          className="invoice__form-location"
-          name="location"
-          placeholder="Location"
-          onChange={e => this.changeHandle(e, "location_id")}>
-          <option value="">Choose a location:</option>
-          {
-            this.state.locationList.map(location => {
-              return(
+        <div className="invoice__form-top">
+          <div className="invoice__form-itemID">
+            {this.state.item_id}
+          </div>
+          <select
+            className="invoice__form-item"
+            name="item"
+            value={this.state.item_id}
+            onChange={e => {this.changeHandle(e, "item_id")}}>
+            <option value="Item ID">Choose an item:</option>
+            {
+              this.state.itemList.map(item => {
+                return(
                 <option
-                  key={location.id}
-                  value={location.id}>
-                  {location.name}
+                  key={item.id}
+                  value={item.id}>
+                  {item.name}
                 </option>
-              )
-            })
-          }
-        </select>
-        <input 
-          className="invoice__form-cases"
-          name="cases"
-          placeholder="#"
-          onChange={e => {
-            this.changeHandle(e, "cases")}}/>
-        <button 
-          className="invoice__form-button"
-          type="submit">Add Cases</button>
+                )
+              })
+            }
+          </select>
+          <button 
+            className="invoice__form-button"
+            type="submit">Add Cases</button>
+        </div>
+        <div className="invoice__form-bottom">
+          <select
+            className="invoice__form-location"
+            name="location"
+            value={this.state.location_id}
+            onChange={e => this.changeHandle(e, "location_id")}>
+            <option value="">Choose a location:</option>
+            {
+              this.state.locationList.map(location => {
+                return(
+                  <option
+                    key={location.id}
+                    value={location.id}>
+                    {location.name}
+                  </option>
+                )
+              })
+            }
+          </select>
+          <input 
+            className="invoice__form-cases"
+            name="cases"
+            value={this.state.cases}
+            placeholder="#"
+            onChange={e => {
+              this.changeHandle(e, "cases")}}/>
+        </div>
       </form>
     </div>
   )
